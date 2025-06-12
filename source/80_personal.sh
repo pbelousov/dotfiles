ac06ff710c407cd401e76ea5cb44cf2b6c6f17c4
Pavel Belousov <mail@pavelbelousov>
2025-06-12 16:20:04 +0200

    Add missed feature

---
+++ source/80_personal.sh
@@ -0,0 +1,7 @@
+# Up/Down Arrow search
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
